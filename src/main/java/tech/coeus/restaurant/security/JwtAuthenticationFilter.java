d58d42a7b193e238dc861ab23fe08964a7ff96a9
nurlanlan <nurahmad004@gmail.com>
2025-04-11 16:09:46 +0400

    Jwt config updated

--- src/main/java/tech/coeus/restaurant/security/JwtAuthenticationFilter.java
+++ src/main/java/tech/coeus/restaurant/security/JwtAuthenticationFilter.java
@@ -13,6 +13,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
